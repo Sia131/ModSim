1f73cfa54e6fff31381cb2c10174dfc7fc92f8cb
Siawash Naqibi <siawash@live.no>
2020-03-01 23:37:16 +0100

    done with 6

--- Assignment 6/Problem2.m
+++ Assignment 6/Problem2.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-eps = 10^(-3);
+eps = 10^(-6);
 %alpha = 10^(-3); %2b
 alpha = 0; %2c
 parameters = [eps; alpha];
@@ -20,27 +20,27 @@ time_final = 15; %Final time
 [time2,statetraj2] = ode15s(@(t,x)ODEfunc2(t, x, parameters),[0,time_final],state_0);
 
 figure('NumberTitle', 'off', 'Name', 'ODE vs DAE');
-sgtitle('2c) \alpha = 0, \epsilon = 10^{-3}')
+sgtitle('2c) \alpha = 0, \epsilon = 10^{-6}')
 
 %ODE plots
 subplot(4,2,1);
-plot(time,statetraj(:,1), 'LineWidth', 2);
+plot(time,statetraj(:,1),'r', 'LineWidth', 2);
 title('ODE solution')
 grid('on');
 ylabel('x1')
 
 subplot(4,2,3);
-plot(time,statetraj(:,2), 'LineWidth', 2);
+plot(time,statetraj(:,2),'b', 'LineWidth', 2);
 grid('on');
 ylabel('x2')
 
 subplot(4,2,5);
-plot(time,statetraj(:,3), 'LineWidth', 2);
+plot(time,statetraj(:,3),'y', 'LineWidth', 2);
 grid('on');
 ylabel('z1')
 
 subplot(4,2,7);
-plot(time,statetraj(:,4), 'LineWidth', 2);
+plot(time,statetraj(:,4), 'g','LineWidth', 2);
 grid('on');
 xlabel('t');
 
@@ -48,19 +48,19 @@ ylabel('z2');
 
 %DAE plots
 subplot(4,2,2);
-plot(time2,statetraj2(:,1), 'LineWidth', 2);
+plot(time2,statetraj2(:,1), 'r','LineWidth', 2);
 title('DAE approximation, epsilon=0')
 grid('on');
 
 subplot(4,2,4);
-plot(time2,statetraj2(:,2), 'LineWidth', 2);
+plot(time2,statetraj2(:,2), 'b','LineWidth', 2);
 grid('on');
 
 subplot(4,2,6);
-plot(time2,statetraj2(:,3), 'LineWidth', 2);
+plot(time2,statetraj2(:,3),'y', 'LineWidth', 2);
 grid('on');
 
 subplot(4,2,8);
-plot(time2,statetraj2(:,4), 'LineWidth', 2);
+plot(time2,statetraj2(:,4),'g', 'LineWidth', 2);
 grid('on');
 xlabel('t');
